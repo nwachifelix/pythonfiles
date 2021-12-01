e8fd98302f3de77f11f1f1cc9194aa2188a97f0e
nwachifelix <ogbonna54615@gmail.com>
2021-12-01 16:28:53 +0100

    A program that outputs even and odd numbers

---
+++ my_lists.py
@@ -0,0 +1,16 @@
+# This is a program that outputs even and od numbers.
+
+my_list = [1, 2, 3, 4, 5, 6, 7, 21, 33, 32, 2, 4]
+
+for num in my_list:
+    if num % 2 == 0:
+        print(num)clear
+        
+
+#Testing for even numbers.
+
+for num in my_list:
+    if num % 2 == 1:
+        print(num)
+
+    
