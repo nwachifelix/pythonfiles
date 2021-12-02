eab4b18f58eb2e8ea6249c746731dcedae372a9e
nwachifelix <ogbonna54615@gmail.com>
2021-12-02 20:37:03 +0100

    This program extracts odd and even numbers from list of numbers

--- my_lists.py
+++ my_lists.py
@@ -1,16 +1,15 @@
-# This is a program that outputs even and od numbers.
+# This is a program that outputs even and odd numbers.
 
 my_list = [1, 2, 3, 4, 5, 6, 7, 21, 33, 32, 2, 4]
-
+print(f'\nThese are the even numbers below. ')
 for num in my_list:
     if num % 2 == 0:
-        print(num)clear
-        
-
+        print(num)
+print(f'\nThe values below are all odd numbers.')
 #Testing for even numbers.
 
 for num in my_list:
-    if num % 2 == 1:
+    if num % 2 != 0:
         print(num)
 
     
