253e76fe9ef16be8f05d04cd21c345c26d2d1c29
nwachifelix <ogbonna54615@gmail.com>
2021-12-07 12:05:52 +0100

    A program on while and for loops with the help of Ugo

---
+++ code_hub_whileloop_test.py
@@ -0,0 +1,28 @@
+# List of numbers
+listOfStuff = [21, 33, 77, 2, 4, 10, 3, 17]  #index 0 - 7
+
+# This while loop starts listing the odd and even from index 7 which is number 17
+my_nums = len(listOfStuff)   #first, determine list length = 8
+count = 0
+print('while loop') #While loop
+while count < my_nums:  #True
+    if listOfStuff[count] % 2 == 0:
+        print(f"even - {listOfStuff[count]}")
+    else:
+        print(f"odd - {listOfStuff[count]}")
+    
+    count = count + 1 #count = 1
+print('-----------------------------------------------------------------')
+
+# this for loop starts listing the odd and even number from index 0 which is 21
+print('for loop')
+listOfStuff = [21, 33, 77, 2, 4, 10, 3, 17]
+for x in listOfStuff:
+    if x % 2 == 0:
+        print(f"even - {x}")
+    else:
+        print(f"odd - {x}")
+    
+# Task:
+# Rewrite the while loop so that it would start listing the odd and even numbers from
+# index 0
