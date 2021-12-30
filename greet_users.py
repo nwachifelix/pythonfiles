d2f99fc417e9d90c249e8aa0d419080ca3d42dc6
nwachifelix <ogbonna54615@gmail.com>
2021-12-30 05:32:55 +0100

    Passing and Modifying in a function.

---
+++ greet_users.py
@@ -0,0 +1,45 @@
+def greet_users(names):
+    '''Print a simple greeting to each user in the list'''
+    for name in names:
+        message = f'Hello, {name.title()}! '
+        print(message)
+usernames = ['vee', 'ugo', 'chima']
+greet_users(usernames)
+
+# Start with some designs that need to be printed.
+unprinted_designs = ['phone case', 'robot pendant', 'dodecagedron'] 
+completed_models = []
+
+# Simulate printing each design, until none are left.
+# Move each design to completed_models after printing. 
+while unprinted_designs:
+    current_designs = unprinted_designs.pop()
+    print(f'Printing model: {current_designs} ')
+    completed_models.append(current_designs)
+
+# Display all completed models.
+print('\nThe following models have been printed:')
+for completed_model in completed_models:
+    print(completed_model)
+
+def print_models(unprinted_designs, completed_models):
+    '''
+    Simulate printing each, until none are left.
+    Move each design to completed_models after printing.
+    '''
+    while unprinted_designs:
+        current_designs = unprinted_designs.capitalize()
+        print(f'Printing model: {current_designs} ')
+        completed_models.append(current_designs)
+
+def show_completed_models(completed_models):
+    '''Show all the models that were printed'''
+    print(f'\nThe following models have been printed:')
+    for completed_model in completed_models:
+        print(completed_model)
+
+unprinted_designs = ['phone case', 'robot pendant', 'dodecahedron']
+completed_models = []
+
+print_models('unprinted_designs', 'completed_models')
+show_completed_models('completed_models')
