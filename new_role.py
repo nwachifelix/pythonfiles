bbe2ff319755cab3d3b4b51484a9b77692636822
nwachifelix <ogbonna54615@gmail.com>
2022-01-01 12:13:24 +0100

    recent version

--- new_role.py
+++ new_role.py
@@ -17,7 +17,7 @@ def show_messages(messages, sent_messages):
 def send_messages(sent_messages):
     print('\nAll the messages have been sent:\n ')
     for sent_message in sent_messages:
-        print(f'{sent_message.capitalize()}')
+        print(f'- {sent_message.capitalize()}')
 
 messages = ['today is a blessing', '2022 is japa year', 'adding more trucks this year', 'taking  care of my health' ]
 sent_messages = []
