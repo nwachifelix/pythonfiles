da73ce58d76304f14dd99b2f8ee3f365f9017091
nwachifelix <ogbonna54615@gmail.com>
2022-01-03 02:28:26 +0100

    current version

--- arbitrary.py
+++ arbitrary.py
@@ -5,13 +5,27 @@ def my_rice_style(*red_oils):
 my_rice_style('white_rice')
 my_rice_style('smoked_fish', 'fresh_pepper', 'red_oil', 'cray_fish', 'etc')
 
-def my_rice_style(*red_oils):
+def my_rice_style(size, *red_oils):
     '''
-    Summarize the rice I'm about to cook
+    Summarize the rice I'm about to cook for entire family
     '''
-    print('\nCooking red_oil rice with below ingredients: ')
+    print(f'\nCooking {size} inch pot red_oil rice with below ingredients: ')
     for red_oil in red_oils:
         print(f'- {red_oil}')
 
-my_rice_style('white_rice')
-my_rice_style('smoked_fish', 'fresh_pepper', 'red_oil', 'cray_fish', 'etc')
+my_rice_style(18, 'white_rice')
+my_rice_style(18, 'smoked_fish', 'fresh_pepper', 'red_oil', 'cray_fish', 'etc')
+
+def build_profile(first, last, **user_info):
+    '''
+    Building a dictionary containing everything we know about a user
+    '''
+    user_info['first_name'] = first
+    user_info['last_name'] = last
+    return user_info
+
+user_profile = build_profile('nwachi', 'felix', 
+                            location='manitoba',
+                            field='data_analysis')
+
+print(user_profile)
