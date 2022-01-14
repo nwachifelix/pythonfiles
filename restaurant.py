05cacee31148050da07c6a71be8283552c008b3b
nwachifelix <ogbonna54615@gmail.com>
2022-01-14 12:25:36 +0100

    recent

--- restaurant.py
+++ restaurant.py
@@ -1,35 +1,35 @@
-# class Restaurant:
-#     '''A class that describes a restaurant and its cuisine.'''
+class Restaurant:
+    '''A class that describes a restaurant and its cuisine.'''
     
-#     def __init__(self, name, cuisine):
-#         '''Activate name and cuisine'''
-#         self.name = name
-#         self.cuisine = cuisine
+    def __init__(self, name, cuisine):
+        '''Activate name and cuisine'''
+        self.name = name
+        self.cuisine = cuisine
 
-#     def describe_restaurant(self):
-#         '''Tells the type of restaurant and the cuisine offered.'''
-#         print(f'{self.name} is a local reataurant that specializes in Nigerian cuisines. ')
+    def describe_restaurant(self):
+        '''Tells the type of restaurant and the cuisine offered.'''
+        print(f'{self.name} is a local reataurant that specializes in Nigerian cuisines. ')
         
 
-#     def open_restaurant(self):
-#         '''Tells the public that we re open for business.'''
-#         print(f'{self.name} is now open to the public. ')
+    def open_restaurant(self):
+        '''Tells the public that we re open for business.'''
+        print(f'{self.name} is now open to the public. ')
 
-# my_fav_restaurant = Restaurant('Abi\'s Bukka', 'Palm oil rice')
-# print(f'\nThe name of the bukka where I have lunch is: {my_fav_restaurant.name}. ')
-# print(f'My favourite meal there is: {my_fav_restaurant.cuisine}. ')
+my_fav_restaurant = Restaurant('Abi\'s Bukka', 'Palm oil rice')
+print(f'\nThe name of the bukka where I have lunch is: {my_fav_restaurant.name}. ')
+print(f'My favourite meal there is: {my_fav_restaurant.cuisine}. ')
 
 
-# print('..........................................................')
-# my_fav_restaurant.open_restaurant()
-# my_fav_restaurant.describe_restaurant()
+print('..........................................................')
+my_fav_restaurant.open_restaurant()
+my_fav_restaurant.describe_restaurant()
 
-# my_wife_restaurant = Restaurant('Mega Chicken', 'fried rice and gizard')
-# print(f'\nMy wife\'s favouriye restaurant is: {my_wife_restaurant.name}. ')
-# print(f'And her best meal there is: {my_wife_restaurant.cuisine} ')
-# print('..........................................................')
-# my_wife_restaurant.describe_restaurant()
-# my_wife_restaurant.open_restaurant()
+my_wife_restaurant = Restaurant('Mega Chicken', 'fried rice and gizard')
+print(f'\nMy wife\'s favouriye restaurant is: {my_wife_restaurant.name}. ')
+print(f'And her best meal there is: {my_wife_restaurant.cuisine} ')
+print('..........................................................')
+my_wife_restaurant.describe_restaurant()
+my_wife_restaurant.open_restaurant()
 
 
 class User:
